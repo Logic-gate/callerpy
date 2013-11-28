6e4521195d4a5bf09767eb9d71f0533b8b98d34b
A'mmer Almadani <mad-dev@SYSBASE>
2013-11-28 17:30:34 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,98 @@
 callerpy
 ========
 
-Truecaller Name Retriever
+Truecaller Name Retriever.
+Since my request for the API was rejected, I commenced using python parsing libraries.  
+Callerpy emulates the process one would encounter if using a web-browser. 
+
+***
+TODO
+--------
+***
+LAST EDIT 28/11/2013
+```  
+CallerPy v0.2 - TODO
+  
+Defintion: 
+
+* [IMP]	    Important Changes 
+* [ASTH]	Aesthetic Implementations  
+* [FIX]	    Fixes to Currant Version  
+* [IMPV]	Improvments
+-----------------------
+1. [IMPV]	More Login Methods--Facebook, G+, Linkedin  
+2. [IMP]	Automated Name|Number Crawler  
+3. [ASTH]	View in Map * Will handle encoding  
+4. [IMP]	Show all possible names  
+5. [IMPV]	Add API feature; ability to utilise CallerPy from other applications  
+6. [IMP]	Define function to handle arguments
+7. [IMP]    Add xml parsing
+```
+***
+How To
+-----
+***
+**CMD**
+```
+python callerpy.py -h
+usage: callerpy_v2.py [-h] -n number [-c country] [-cc country code] -l login
+
+TrueCaller Name Retriever
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n number, --number number
+                        Phone Number Without Country Code (default: None)
+  -c country, --country country
+                        Country | String (default: None)
+  -cc country code, --countrycode country code
+                        Country | Int (default: None)
+  -l login, --login login
+                        Login Method | twitter, g+, fb (default: twitter)
+
+Do not forget to hardcode your credentials
+```
+***
+**RUN**
+```
+python callerpypy -n 7349303030 -c us -l twitter  
+```
+***
+*OR*
+```
+python callerpypy -n 7349303030 -c 1 -l twitter
+```
+***
+**OUTPUT**
+```
+            <entry>
+    			<name>Dominos Pizzay Quejas</name>
+				<number>7349303030</number>
+				<country>us</country>
+			</entry>
+		
+			<entry>
+				<name>Pizza Hut Delivery</name>
+				<number>08447700669</number>
+				<country>uk</country>
+			</entry>
+
+```
+*Note*: you should enclose the xml by adding a tag
+```
+<something>
+            <entry>
+        		<name>Dominos Pizzay Quejas</name>
+				<number>7349303030</number>
+				<country>us</country>
+			</entry>
+		
+			<entry>
+				<name>Pizza Hut Delivery</name>
+				<number>08447700669</number>
+				<country>uk</country>
+			</entry>
+</something>
+```
+
+
---
+++ callerpy.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+# CallerPy V 2.0 - Truecaller Name Retriever
+# Copyright (C) <2013>  mad_dev(A'mmer Almadani)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Report any issues with this script to <mad_dev@linuxmail.org>
+
+
+import mechanize
+from bs4 import BeautifulSoup
+import re
+import datetime
+import argparse
+
+
+#####CREDS_twitter#####
+user = ''
+pwd = ''
+
+class CallerPy():
+
+	def authenticate(self):
+		url = 'http://www.truecaller.com/authenticate/'
+		return url
+
+	def twitter(self, user, pwd):
+		
+		param = 'twitter'
+		url = self.authenticate()
+		B.open(url+param)
+		B.form = list(B.forms())[0]
+		B['session[username_or_email]'] = user
+		B['session[password]'] = pwd
+		return B.submit()
+
+	#def googlePlus():
+
+	#def facebook():
+	
+
+	def truecaller(self, country, number):
+		B.form = list(B.forms())
+		B.follow_link(nr=1)
+		q = B.open('http://www.truecaller.com/search/%s/%s' %(country, number))
+		a = q.read()
+		bs = BeautifulSoup(a)
+		if '<h2 id="profile-name">' in a:
+			for name in bs.find_all('h2',{"id":"profile-name"}):
+				nm = re.split(r'<.*?>', str(name))
+				for i in nm:
+					print i
+		self.save(nm[1], country, number)
+
+
+	def save(self, name, country, number):
+		template = '''
+			<entry>
+				<name>%s</name>
+				<number>%s</number>
+				<counrty>%s</country>
+			</entry>
+		''' %(name, number, country)
+		xml = open('log.xml', 'a')
+		xml.write(template)
+		xml.close()
+
+	def country_by_code(self, code):
+		a = open('countries', 'r')
+		b = a.read()
+		p = re.findall('[a-z]{1,}', b)
+		o = re.findall('[0-9]{1,}', b)
+		for i, e in zip(p, o):
+			if str(code) in e:
+				return i
+
+
+if __name__ == '__main__':
+	par = argparse.ArgumentParser(prog=__file__, formatter_class=argparse.ArgumentDefaultsHelpFormatter, 
+		epilog="Do not forget to hardcode your credentials", description='TrueCaller Name Retriever')
+	par.add_argument('-n', '--number', required=True, help="Phone Number Without Country Code", metavar='number')
+	par.add_argument('-c', '--country',required=False,  help="Country | String", metavar='country')
+	par.add_argument('-cc', '--countrycode',required=False,  help="Country | Int", metavar='country code', type=int)
+	par.add_argument('-l', '--login', required=True, default='twitter', help="Login Method | twitter, g+, fb", metavar='login')
+
+	argvs = par.parse_args()
+
+	current_date = datetime.datetime.now()
+	
+	B = mechanize.Browser()
+	B.set_handle_robots(False)
+	x = CallerPy()
+	
+	'''TODO:Define function to handle arguments'''
+	if argvs.country is None:
+		if argvs.login == 'twitter':
+			print 'Using Twitter'
+			x.twitter(user, pwd), x.truecaller(x.country_by_code(argvs.countrycode), argvs.number)
+		else:
+			print 'This version only supports twitter'
+	if argvs.countrycode is None:
+		if argvs.login == 'twitter':
+			print 'Using Twitter'
+			x.twitter(user, pwd), x.truecaller(argvs.country, argvs.number)
+		else:
+			print 'This version only supports twitter'
+
+
+
+#http://www.truecaller.com/search/saudiarabia/0503297977
